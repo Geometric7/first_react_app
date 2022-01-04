93e68e86d1a582d2ad970f3f225778162d2d37b7
Marcin Dzikowicz <mistamasta7@gmail.com>
2022-01-04 12:10:31 +0100

    Add Cards

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,19 @@
 import styles from './Button.module.scss'
 
-const Button = props => {
-  return (<button className={styles.button}>{props.children}</button>);
+const Button = ({children,...rest}) => {
+  return (<button className={styles.button}>{children}</button>); //strzałkowa - blokowa
 }
+
+/*const Button2 = props => (<button Data-x="1" className={styles.button}>{props.children}</button>); // strzałkowa - liniowa
+
+
+const Button3 = function (props) {
+  return (<button Data-y="1" className={styles.button}>{props.children}</button>); //anoninowa
+};
+
+function Button(props) {
+  return (<button Data-z="1" className={styles.button}>{props.children}</button>); //nazwana
+}*/
+
 
 export default Button;
--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,5 +1,6 @@
 import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
+import Button from '../Button/Button';
 
 const ColumnForm = props => {
   const [value, setValue] = useState('');
@@ -10,14 +11,31 @@ const ColumnForm = props => {
     setIcon('');
   };
 
+  const handleSubmit2 = function(e) {
+    e.preventDefault();
+    props.action({ title: title, icon: icon});
+    setValue('');
+    setIcon('');
+  };
+
   const [title, setTitle] = useState('');
   const [icon, setIcon] = useState('');
 
   return (
-    <form onSubmit={handleSubmit}>
-          Title: <input type="text" value={title} onChange={e => setTitle(e.target.value)} />
-          Icon: <input type="text" value={icon} onChange={e => setIcon(e.target.value) }/>
-          <button>Add column</button>
+    <form className={styles.columnForm} onSubmit={handleSubmit}>
+          <span className={styles.columnFormSpan}>Title: </span>
+          <input className={styles.columnFormTitle}
+                  type="text"
+                  value={title}
+                  onChange={e => setTitle(e.target.value)}
+          />
+          <span className={styles.columnFormSpan}>Icon: </span>
+          <input className={styles.columnFormInput}
+                 type="text"
+                 value={icon}
+                 onChange={e => setIcon(e.target.value)}
+          />
+    <Button className={styles.columnFormButton}>Add column</Button>
     </form>
   );
 };
