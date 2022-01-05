bd7d7025f08dc630adc5638ac8f84c056d757097
Marcin Dzikowicz <mistamasta7@gmail.com>
2022-01-05 12:03:06 +0100

    Edit CardForm in Columns.js

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,14 +3,14 @@ import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
 
 const Column = props => {
-  
+
     return (
         <article className={styles.column}>
             <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon}></span>{props.title}</h2>
             <ul className={styles.cards}>
                 {props.cards.map(card => <Card key={card.id} title={card.title} /*Column={Column.id}*/ />)}
             </ul>
-            <CardForm columnId={props.id} action={props.columnId} action = {props.action} />
+            <CardForm columnId={props.id} action = {props.action} />
         </article>
     );
 };
