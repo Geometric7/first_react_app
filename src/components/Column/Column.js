f09613167969992d8224c2f85875bc1a2358e419
Marcin Dzikowicz <mistamasta7@gmail.com>
2022-01-17 02:01:47 +0100

    Add reducer & 'switch ...case' cleanup

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -17,7 +17,7 @@ const Column = props => {
             <ul className={styles.cards}>
                 {cards.map(card => <Card key={card.id} title={card.title} /*Column={Column.id}*/ />)}
             </ul>
-            <CardForm columnId={props.id} action = {props.action} />
+            <CardForm columnId={props.id} />
         </article>
     );
 };
